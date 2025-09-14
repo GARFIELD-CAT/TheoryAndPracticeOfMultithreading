56a39e88077edc230be074e8328e928bf46c6919
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-09-14 21:02:08 +0300

    Task completed HW 1

--- src/main/java/ru/yagunov/demo/CurrencyExchangeRateParserAppApplication.java
+++ src/main/java/ru/yagunov/demo/CurrencyExchangeRateParserAppApplication.java
@@ -5,10 +5,60 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CurrencyExchangeRateParserAppApplication {
+    public static class TaskService {
+        public void startTask(Runnable task) {
+            Thread thread = new Thread(task);
+            thread.start();
+        }
+    }
+
+    public static class Task implements Runnable{
+        @Override
+        public void run() {
+            Thread currentThread = Thread.currentThread();
+            currentThread.setName("ServiceWorker");
+
+            System.out.println(
+                "Имя потока: " + currentThread.getName() + ", Id потока: " + currentThread.getId()
+            );
+
+            System.out.println("Поток " + currentThread.getName() + " завершил работу!");
+        }
+    }
 
 	public static void main(String[] args) {
         SpringApplication.run(CurrencyExchangeRateParserAppApplication.class, args);
-        System.out.println("Мой первый код!");
-	}
+        System.out.println("Главный поток работает!");
 
+        Thread logger = new Thread(() -> {
+            Thread currentThread = Thread.currentThread();
+            currentThread.setName("LoggerThread");
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(
+                    "Имя потока: " + currentThread.getName() + ", Id потока: " + currentThread.getId() + ": " + i
+                );
+
+                try {
+                    Thread.sleep(1000); // Задержка в 1 секунду
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            System.out.println("Поток " + currentThread.getName() + " завершил работу!");
+        });
+
+        Runnable task = new Task();
+
+        TaskService service = new TaskService();
+        service.startTask(task);
+        service.startTask(logger);
+
+        Thread.getAllStackTraces().keySet().forEach(thread -> {
+            System.out.println("Active Thread: " + thread.getName());
+        });
+
+        System.out.println("Главный поток завершил работу!");
+	}
 }
