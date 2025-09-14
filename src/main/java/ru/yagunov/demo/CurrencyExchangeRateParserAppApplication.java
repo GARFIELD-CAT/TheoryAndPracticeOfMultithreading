af995684e9dc9620984852f8fd3a80f57615f1fb
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-09-14 20:26:55 +0300

    Add a basic project configuration to spring

---
+++ src/main/java/ru/yagunov/demo/CurrencyExchangeRateParserAppApplication.java
@@ -0,0 +1,14 @@
+package ru.yagunov.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeRateParserAppApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeRateParserAppApplication.class, args);
+        System.out.println("Мой первый код!");
+	}
+
+}
---
+++ src/test/java/ru/yagunov/demo/CurrencyExchangeRateParserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yagunov.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeRateParserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
